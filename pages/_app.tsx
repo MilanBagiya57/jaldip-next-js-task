7495a371e44cf469e1b7c85a9f0fe724acd061ca
milan_appunik <milan.bagiya@appunik.com>
2023-09-05 16:59:11 +0530

    css changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,20 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import './../styles/globals.css';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
-  <div >
-        <Head>
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-        />
+  return (
+    <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:wght@100;200;300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500&display=swap"
           rel="stylesheet"
         />
       </Head>
-   {/* <Header/> */}
-    <Component {...pageProps} />
-  </div>
-  )
+      {/* <Header/> */}
+      <Component {...pageProps} />
+    </div>
+  );
 }
